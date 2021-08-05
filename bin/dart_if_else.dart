71d9fccd802f84a13bbf6b23facd22227e64c0f2
fabiojorge-fisico-desenvolvedor <fabiomendes@edu.se.df.gov.br>
2021-08-05 08:32:24 -0300

    primeiro comando

--- bin/dart_if_else.dart
+++ bin/dart_if_else.dart
@@ -1,5 +1,12 @@
 import 'package:dart_if_else/dart_if_else.dart' as dart_if_else;
+import 'dart:io';
 
+//solicitar nota e se for maior imprime aprovado, senão reprovado e o que falta
 void main(List<String> arguments) {
-  print('Hello world: ${dart_if_else.calculate()}!');
+  print('Digite Sua Nota: ');
+  var entradaNota = stdin.readLineSync();
+  var nota = double.parse(entradaNota!);
 }
+
+
+void main (List<String>arguments)
