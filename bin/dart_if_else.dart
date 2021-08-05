85a1b917ede3b6b069b070d0692f9de5563fdb56
fabiojorge-fisico-desenvolvedor <fabiomendes@edu.se.df.gov.br>
2021-08-05 08:25:59 -0300

    teste func

---
+++ bin/dart_if_else.dart
@@ -0,0 +1,5 @@
+import 'package:dart_if_else/dart_if_else.dart' as dart_if_else;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_if_else.calculate()}!');
+}
---
+++ test/dart_if_else_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_if_else/dart_if_else.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
