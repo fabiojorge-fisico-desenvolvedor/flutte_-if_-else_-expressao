5e011f23eab6155a6726e404dfa7dc84e61243b4
fabiojorge-fisico-desenvolvedor <fabiomendes@edu.se.df.gov.br>
2021-08-05 10:02:42 -0300

    operadores relacionais

--- bin/dart_if_else.dart
+++ bin/dart_if_else.dart
@@ -2,11 +2,14 @@ import 'package:dart_if_else/dart_if_else.dart' as dart_if_else;
 import 'dart:io';
 
 //solicitar nota e se for maior imprime aprovado, senão reprovado e o que falta
+
+// experimentando operadores relacionais // > < > =
 void main(List<String> arguments) {
   print('Digite Sua Nota: ');
   var entradaNota = stdin.readLineSync();
   var nota = double.parse(entradaNota!);
-  if (nota < 6) {
+  if (nota <= 5.99) {
+    //experimentando operadores relacionais <, >, =
     print('você reprovou!');
     var notaQueFaltou = 6 - nota;
     print('você reprovou! faltou ${notaQueFaltou}');
